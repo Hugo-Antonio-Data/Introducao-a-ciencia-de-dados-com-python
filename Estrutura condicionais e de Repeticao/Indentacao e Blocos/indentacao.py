52e2e73eec15afd4af6414c04e1ba9c219fff62d
Hugo-Antonio <hugoantonio182@outlook.com>
2024-04-23 01:14:30 -0300

    Aula 01- Indentacao e Blocos

---
+++ Estrutura condicionais e de Repeticao/Indentacao e Blocos/indentacao.py
@@ -0,0 +1,12 @@
+def sacar(valor):
+    saldo = 200
+    if saldo >= valor:
+        print('Valor sacado')
+        print('Retire o dinheiro na boca do caixa')
+        print(f'Seu saldo atual é: R$ {saldo - valor}')
+    else:
+        print(f'Saldo é menor que o valor solicitado, digite um valor abaixo de seu Saldo que é de R${saldo}')
+
+
+saque_caixa = float(input('Digite o valor que deseja sacar: R$ '))
+sacar(saque_caixa)
