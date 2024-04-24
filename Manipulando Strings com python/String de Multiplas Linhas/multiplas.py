348e9d12f330326429f6fe782be043ab34532e80
Hugo-Antonio <hugoantonio182@outlook.com>
2024-04-24 01:19:26 -0300

    Aula 04 - Strings de multiplas linhas

---
+++ Manipulando Strings com python/String de Multiplas Linhas/multiplas.py
@@ -0,0 +1,13 @@
+nome = str(input('Digite seu nome: '))
+mensagem = f'''
+Meu nome é {nome} e estou aprendendo python
+até mais!
+'''
+print(mensagem)
+
+dados = '''
+==================Obrigado===================
+        #Terminando mais um modulo#
+=================FIM=========================
+'''
+print(dados)
