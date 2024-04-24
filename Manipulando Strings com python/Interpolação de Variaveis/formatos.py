8a630e43c8be6510746832c7acc35d068311882c
Hugo-Antonio <hugoantonio182@outlook.com>
2024-04-24 00:32:34 -0300

    Aula 03 - Fatiamento de String

---
+++ Manipulando Strings com python/Interpolação de Variaveis/formatos.py
@@ -0,0 +1,7 @@
+nome = str(input('Digite seu nome: '))
+sobrenome = str(input('Digite seu sobrenome: '))
+idade = int(input('Idade: '))
+saldo = float(input('Digite seu saldo em banco: '))
+
+print('Seu nome é {}, seu sobrenome é {}, sua idade é {} e seu saldo é {:.2f}.'.format(nome, sobrenome, idade, saldo))
+print(f'Seu nome é {nome} , seu sobrenome é {sobrenome}, sua idade é {idade} seu saldo é {saldo:.2f}.')
